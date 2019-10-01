ccaefeb06c3f3139e3a67b0ebc1ae0e78b219ffa
Jacob Harrington <jharrington4@gmail.com>
2019-10-01 11:17:53 -0700

    added percentage to test results

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import { isYes } from './functions.js';
 const myButton = document.getElementById('action-button');
 const testResults = document.getElementById('quiz-answers');
+let testScore = 0;
 
 myButton.addEventListener('click', () => {
     alert('Welcome to the best quiz ever!');
@@ -13,6 +14,7 @@ myButton.addEventListener('click', () => {
     const answerOne = document.getElementById('question-one');
     const functionAnswerOne = isYes(sanitizedUserAnswerOne);
     answerOne.textContent = sanitizedUserAnswerOne + ' and that is ' + functionAnswerOne;
+    if(sanitizedUserAnswerOne === 'yes') testScore ++;
 
     const userAnswerTwo = prompt(userName + ', does Jacob like video games?');
     const sanitizedUserAnswerTwo = userAnswerTwo
@@ -21,6 +23,7 @@ myButton.addEventListener('click', () => {
     const answerTwo = document.getElementById('question-two');
     const functionAnswerTwo = isYes(sanitizedUserAnswerTwo);
     answerTwo.textContent = sanitizedUserAnswerTwo + ' and that is ' + functionAnswerTwo;
+    if(sanitizedUserAnswerTwo === 'yes') testScore ++;
 
     const userAnswerThree = prompt(userName + ', is Stella a cat?');
     const sanitizedUserAnswerThree = userAnswerThree
@@ -29,8 +32,19 @@ myButton.addEventListener('click', () => {
     const answerThree = document.getElementById('question-three');
     const functionAnswerThree = isYes(sanitizedUserAnswerThree);
     answerThree.textContent = sanitizedUserAnswerThree + ' and that is ' + functionAnswerThree;
+    if(sanitizedUserAnswerThree === 'yes') testScore ++;
+    
+    const testPercent = document.getElementById('test-score');
+    const uniqueTestScore = Math.floor((testScore / 3) * 100);
+    testPercent.textContent = uniqueTestScore;
 
     alert('Your test results will display below the "Take My Quiz" button!')
+
+    if(testScore === 3) {
+        document.getElementById('test-score').style.cssText = 'color:green';
+    } else { 
+        document.getElementById('test-score').style.cssText = 'color:red';
+    }
 
     isYes(sanitizedUserAnswerOne);
     isYes(sanitizedUserAnswerTwo);
